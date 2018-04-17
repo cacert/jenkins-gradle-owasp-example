d1fd20df8d4fc33581eec5cc6af832a7a7a6e656
Kasim Sert <kasim.sert@zalando.de>
2018-04-17 10:56:07 +0200

    ok

---
+++ src/test/java/com/example/jenkinsgradle/JenkinsGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
